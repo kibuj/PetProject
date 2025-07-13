ce57379c4d1f237003a72dad89fce1427be6bdfc
bogdan <bogdankibuj@gmail.com>
2025-07-14 02:09:14 +0300

    add cars models

--- onlineshop/catalog/models.py
+++ onlineshop/catalog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class CarBrand(models.Model):
+    brand = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.brand
+
+class CarModel(models.Model):
+    brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.brand + ' ' + self.name
+
+class Car(models.Model):
+    brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE)
+    name = models.ForeignKey(CarModel, on_delete=models.CASCADE)
+    engine = models.CharField(max_length=30)
+    body = models.CharField(max_length=30)
+    year = models.IntegerField()
+    price = models.IntegerField()
+    mileage = models.PositiveIntegerField(help_text="Пробіг автомобіля")
+    fuel_type = models.CharField(max_length=20, choices={
+        'diesel': 'Diesel',
+        'petrol': 'Petrol',
+        'electric': 'Electric',
+        'hybrid': 'Hybrid',
+        'gas': 'Gas',
+    }
+                                 )
+    transmission = models.CharField(max_length=20, choices={
+        'manual': 'Manual',
+        'automatic': 'Automatic',
+    })
+    image = models.ImageField(upload_to='car_images/')
+    status = models.CharField(max_length=20, choices={
+        'in stock': 'In stock',
+        'expiring': 'Expiring',
+        'out of stock': 'Out of stock',
+    })
